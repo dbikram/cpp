4af2fad5ebc0bc2950078547c1b70919641d85ab
BIKRAM <69245868+dbikram@users.noreply.github.com>
2021-04-09 14:30:35 +0530

    Add files via upload

---
+++ PATTERN5.cpp
@@ -0,0 +1,22 @@
+/* program to print inverted half pyramid using number
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2
+1
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+                int rows;
+                cin>>rows; //enter the rows
+
+                for(int i=rows;i>=1;i--){
+                                for(int j=1;j<=i;j++){
+                                                cout<<j<<" ";
+                                }
+                                cout<<endl;
+                }
+                return 0;
+}
